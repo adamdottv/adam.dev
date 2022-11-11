d53a96c3260b0a98c08753b432c07422573c7d0f
adamelmore <elmore.adam@gmail.com>
2022-11-11 07:46:11 -0600

    feat: improved styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from "@astrojs/cloudflare"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: cloudflare()
-});
+  adapter: cloudflare(),
+})
